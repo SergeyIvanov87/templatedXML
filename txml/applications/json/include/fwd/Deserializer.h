8f0a15ffc4fe4ca7c07c2eae1389bed8c7fb6b25
SergeyIvanov87 <sergius140608@rambler.ru>
2021-10-26 11:09:53 +0300

    Remove example json deserializer

--- txml/applications/json/include/fwd/Deserializer.h
+++ txml/applications/json/include/fwd/Deserializer.h
@@ -16,6 +16,7 @@ struct FromJSON : public txml::FormatDeserializerBase<Impl, txml::StaticCheckUns
     using json = nlohmann::json;
     FromJSON(json &obj);
 
+    // default deserialization routine
     template<class DeserializedItem, class Tracer>
     std::shared_ptr<DeserializedItem> deserialize_impl(txml::details::SchemaDTag<DeserializedItem>, Tracer tracer);
 
@@ -56,68 +57,14 @@ protected:
 
     template<class NodeType, class Tracer>
     static bool check_node_param(json::iterator& cur_it, const json::iterator& cur_end_it,
-                                 json::value_t expected_type, Tracer tracer)
-    {
-        if (cur_it == cur_end_it)
-        {
-            tracer.trace("EOF");
-            return false;
-        }
-
-        const std::string &key = cur_it.key();
-        const auto& value = cur_it.value();
-
-        tracer.trace("Found '", key, "', value type: ", json_type_to_cstring(value.type()), ", value:\n", value);
-        if (value.type() != expected_type || key != NodeType::class_name())
-        {
-            tracer.trace("Expected '", NodeType::class_name(), "', type: ", json_type_to_cstring(expected_type));
-            return false;
-        }
-        return true;
-    }
+                                 json::value_t expected_type, Tracer tracer);
 
     template<class NodeType, class Tracer>
     static bool check_array_node_param(json::iterator& cur_it, const json::iterator& cur_end_it,
-                                       json::value_t expected_type, Tracer tracer)
-    {
-        if (cur_it == cur_end_it)
-        {
-            tracer.trace("EOF");
-            return false;
-        }
-
-        const auto& value = cur_it.value();
-
-        tracer.trace("Found: array element, value type: ", json_type_to_cstring(value.type()), ", value:\n", value);
-        if (value.type() != expected_type)
-        {
-            tracer.trace("Expected '", NodeType::class_name(), "', type: ", json_type_to_cstring(expected_type));
-            return false;
-        }
-        return true;
-    }
+                                       json::value_t expected_type, Tracer tracer);
 
     template<class NodeType, class Tracer>
-    std::shared_ptr<NodeType> create_deserialized_node(Tracer tracer, size_t available_item_count)
-    {
-        std::shared_ptr<NodeType> ret = std::make_shared<NodeType>();
-        tracer.trace("Create node '", NodeType::class_name(), "' handle: ",
-                     ret.get());
-
-        size_t deserialized_item_count = ret->format_deserialize_elements(*this, tracer);
-        while (deserialized_item_count != available_item_count)
-        {
-            tracer.trace("refill node '", NodeType::class_name(), "' handle: ",
-                     ret.get(), " deserialized count: ", deserialized_item_count);
-            size_t next_portion_items = ret->format_deserialize_elements(*this, tracer);
-            if (deserialized_item_count == next_portion_items)
-            {
-                break; //nothing more
-            }
-            deserialized_item_count = next_portion_items;
-        }
-        return ret;
-    }
+    std::shared_ptr<NodeType> create_deserialized_node(Tracer tracer, size_t available_item_count);
 };
 }
 #endif // TXML_APPLICATION_JSON_FWD_DESERIALIZER_H
